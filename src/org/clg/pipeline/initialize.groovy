3f09e6a215bd5580b15fef3a78ee21dc2e152e1a
Charro Gruver <charro.gruver@odfl.com>
2020-04-28 08:56:21 -0400

    Fix initialize pipeline for self-signed certs

--- src/org/clg/pipeline/initialize.groovy
+++ src/org/clg/pipeline/initialize.groovy
@@ -1,9 +1,19 @@
 package org.clg.pipeline
 
-def start() {
+def start(def params) {
 
   openshift.withCluster() {
     openshift.withProject() {
+      openshift.raw("label secret ${params.gitSecret} credential.sync.jenkins.openshift.io=true --overwrite")
+      def namespace = openshift.project()
+      stage('Checkout') {
+        try {
+          git url: "${params.pipelineCodeGitUrl}", branch: "${params.pipelineCodeGitBranch}", credentialsId: "${namespace}-${params.gitSecret}"
+        } catch (Exception e) {
+          sh "git config http.sslVerify false"
+          git url: "${params.pipelineCodeGitUrl}", branch: "${params.pipelineCodeGitBranch}", credentialsId: "${namespace}-${params.gitSecret}"
+        }
+      }
       stage("Apply Pipeline Configs") {
         openshift.apply( openshift.process( "openshift//pipeline-objects" ))
       }
